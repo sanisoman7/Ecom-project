cc10c4192322daad666317d3a5333186366c5bf3
sanisoman7 <sanisoman7@gmail.com>
2022-08-20 13:28:53 +0530

    update code

--- avodhashop/settings.py
+++ avodhashop/settings.py
@@ -130,3 +130,16 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 django_heroku.settings(locals())
+
+
+
+
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- avodhashop/urls.py
+++ avodhashop/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/',include('cart.urls')),
@@ -27,3 +28,7 @@ urlpatterns = [
 ]
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
